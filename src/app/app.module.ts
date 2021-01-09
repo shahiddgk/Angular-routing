7e8e540ddedfe1695e81251d1ab7fc705f8b510c
shahiddgk <mshahid_ms@yahoo.com>
2021-01-09 18:37:05 +0500

    Initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ServersService } from './servers/servers.service';
 const appRoute: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent },
 ];
 
