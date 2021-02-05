aba321900a25236887dcf88a2a52abd08a6d18c2
shahiddgk <mshahid_ms@yahoo.com>
2021-02-05 19:58:37 +0500

    Initial Setup

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -10,10 +11,16 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.router.snapshot.params['id']
+    this.server = this.serversService.getServer(id);
+    this.router.params.subscribe(
+      (params: Params) => {
+        this.serversService.getServer(+params.id)
+      }
+    )
   }
 
 }
