42b4f728406a5213ee60226c86766db96e64a1c1
shahiddgk <mshahid_ms@yahoo.com>
2021-02-08 23:59:00 +0500

    Initial Setup

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -18,7 +18,7 @@ export class ServerComponent implements OnInit {
     this.server = this.serversService.getServer(id);
     this.router.params.subscribe(
       (params: Params) => {
-        this.serversService.getServer(+params.id)
+        this.server = this.serversService.getServer(+params.id)
       }
     )
   }
